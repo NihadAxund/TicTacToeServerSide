99126dd828e61171fbd29478a27e9515b8395da2
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-02-01 00:49:15 +0400

    son

--- TicTacToeServerSide/Services/NetworkService.cs
+++ TicTacToeServerSide/Services/NetworkService.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using TicTacToeServerSide.Help;
 
 namespace TicTacToeServerSide.Services
 {
@@ -16,6 +18,7 @@ namespace TicTacToeServerSide.Services
         private const int PORT = 27001;
         private static readonly byte[] buffer = new byte[BUFFER_SIZE];
         private static bool GameStart = false;
+        private static List<UserLogin> ULLIST { get; set; } = new List<UserLogin>();
         public static bool IsFirst { get; private set; } = false;
         public static char[,] Points = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
 
@@ -24,7 +27,6 @@ namespace TicTacToeServerSide.Services
             Console.Title = "Server";
            
                 SetupServer();
-            
             Console.ReadLine();
             CloseAllSockets();
         }
@@ -51,6 +53,34 @@ namespace TicTacToeServerSide.Services
                 else return false;
             }
             return true;
+        }
+        private static bool XOBole1(char t)
+        {
+            bool isok = false;
+            for (int i = 0; i < Points.Length / 3; i++) //0 //0 //1
+            {
+                for (int j = 0; j < Points.Length / 3; j++)//0 //1
+                {
+                    if (Points[j, i] == t) isok = true;
+                    else
+                    {
+                        isok = false;
+                        break;
+                    }
+                }
+                if (!isok)
+                {
+                    continue;
+                }
+                else
+                {
+                    //Console.ForegroundColor = ConsoleColor.Green;
+                    //Console.WriteLine(t + "Salam");
+                    return isok;
+                }
+            }
+            return false;
+
         }
         private static bool XOBole(char t)
         {
@@ -92,21 +122,24 @@ namespace TicTacToeServerSide.Services
 
         private static void SetupServer()
         {
-            Console.WriteLine("Setting up server . . . ");
-            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
-            serverSocket.Listen(2);
-            while (true)
+            Task.Run(() =>
             {
-                try
+                Console.WriteLine("Setting up server . . . ");
+                serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+                serverSocket.Listen(2);
+                while (true)
                 {
-                     serverSocket.BeginAccept(AcceptCallBack, null);
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
+                    try
+                    {
+                        serverSocket.BeginAccept(AcceptCallBack, null);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
 
-            }
+                }
+            });
         }
 
         private static void AcceptCallBack(IAsyncResult ar)
@@ -133,7 +166,7 @@ namespace TicTacToeServerSide.Services
             {
                 IsFirst = false;
                 t = "O";
-                GameStart = true;
+               // GameStart = true;
                 
             }
             byte[] data = Encoding.ASCII.GetBytes(t+"\t"+IsFirst.ToString());
@@ -144,36 +177,61 @@ namespace TicTacToeServerSide.Services
             //}
         }
 
+        private static bool Boolresult(char symbol)
+        {
+            if (XOBole3(symbol) || XOBole2(symbol))
+                return true;
+            else if (XOBole(symbol) || XOBole1(symbol))
+                return true;
+           return false;
+        }
+
+        static bool IsOkay = false;
         private static  void ReceiveCallback(IAsyncResult ar)
         {
-           bool IsOkay = false;
-            Socket current = (Socket)ar.AsyncState;
-            int received;
+            lock(clientSockets)
+            {
+                bool isok1 = true;
+                Socket current = (Socket)ar.AsyncState;
+                int received;
 
-            try
-            {
-                received = current.EndReceive(ar);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Client forcefully disconnected");
-                current.Close();
-                clientSockets.Remove(current);
-                return;
-            }
-            byte[] recBuf = new byte[received];
-            Array.Copy(buffer, recBuf, received);
-            string text = Encoding.ASCII.GetString(recBuf);
-            if (!GameStart)
-            {
-                current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
-                return;
-            }
-            try
-            {
+                try { received = current.EndReceive(ar); }
+                catch (Exception)
+                {
+                    Console.WriteLine("Client forcefully disconnected");
+                    current.Close();
+                    clientSockets.Remove(current);
+                    return;
+                }
+                byte[] recBuf = new byte[received];
+                Array.Copy(buffer, recBuf, received);
+                string text = Encoding.ASCII.GetString(recBuf);
+                if (isok1 && !GameStart)
+                {
+                    isok1 = false;
+                    if (ULLIST.Count > 0)
+                    {
+                        GameStart = true;
+                    }
+                    Console.WriteLine("Dusdu buraya");
+                    var USer = JsonSerializer.Deserialize<UserLogin>(text);
+                    ULLIST.Add(USer);
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(USer.UserName);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+                    return;
+                }
+                if (!GameStart)
+                {
+                    current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+                    return;
+                }
+                try
+                {
                     var no = text[0];
                     var symbol = text[1];
-                    //Console.WriteLine("Budu: "+no+"||"+symbol);
+                    Console.WriteLine("Budu: " + no + "||" + symbol);
                     var number = Convert.ToInt32(no) - 49;
                     if (number >= 0 && number <= 2)
                         Points[0, number] = symbol;
@@ -181,94 +239,85 @@ namespace TicTacToeServerSide.Services
                         Points[1, number - 3] = symbol;
                     else if (number >= 6 && number <= 8)
                         Points[2, number - 6] = symbol;
-                    if ( XOBole(symbol)|| XOBole3(symbol)|| XOBole2(symbol))
+                    if (Boolresult(symbol))
                     {
-                        IsOkay= true;
+                        IsOkay = true;
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("Udu: "+symbol);
+                        Console.WriteLine("WINER: " + symbol);
                         Console.ForegroundColor = ConsoleColor.White;
                     }
-                 //   Console.WriteLine(no+" | "+symbol);
-                
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int k = 0; k < 3; k++)
-            //    {
-            //        Console.Write($"{Points[i, k]}");
-            //    }
-            //    Console.WriteLine();
-            //    Console.WriteLine();
-            //}
-
-
-            if (text != String.Empty)
-            {
-                var mydata = ConvertString(Points);
-                foreach (var item in clientSockets)
+                }
+                catch (Exception ex)
                 {
-                    if (item==current)
-                    {
-                        if (IsOkay)
-                        {
-                            byte[] data = Encoding.ASCII.GetBytes(mydata + "True\nTrue");
-                            item.Send(data);
-                        }
-                        else
-                        {
-                            byte[] data = Encoding.ASCII.GetBytes(mydata+"True");
-                            item.Send(data);
-                        }
-                    }
-                    else
-                    {
-                        if (IsOkay)
-                        {
-                            byte[] data = Encoding.ASCII.GetBytes(mydata + "True\nFalse");
-                            item.Send(data);
-                        }
-                        else
-                        {
-                            byte[] data = Encoding.ASCII.GetBytes(mydata+"False");
-                            item.Send(data);
-                        }
-                    }
+                    Console.WriteLine(ex.Message);
+                }
 
-                    Console.WriteLine($"Data sent to {item.RemoteEndPoint}");
+
+
+                if (text != String.Empty)
+                {
+                    var mydata = ConvertString(Points);
+                    foreach (var item in clientSockets)
+                    {
+                        if (item == current)
+                        {
+                            if (IsOkay)
+                            {
+                                byte[] data = Encoding.ASCII.GetBytes(mydata + "True\nTrue");
+                                item.Send(data);
+                            }
+                            else
+                            {
+                                byte[] data = Encoding.ASCII.GetBytes(mydata + "True");
+                                item.Send(data);
+                            }
+                        }
+                        else
+                        {
+                            if (IsOkay)
+                            {
+                                byte[] data = Encoding.ASCII.GetBytes(mydata + "True\nFalse");
+                                item.Send(data);
+                            }
+                            else
+                            {
+                                byte[] data = Encoding.ASCII.GetBytes(mydata + "False");
+                                item.Send(data);
+                            }
+                        }
+
+                        Console.WriteLine($"Data sent to {item.RemoteEndPoint}");
+                    }
+                }
+                else if (text == "exit")
+                {
+                    current.Shutdown(SocketShutdown.Both);
+                    current.Close();
+                    clientSockets.Remove(current);
+                    Console.WriteLine($"{current.RemoteEndPoint} disconnected");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Text is an invalid request");
+                    byte[] data = Encoding.ASCII.GetBytes("Invalid Request");
+                    current.Send(data);
+                    Console.WriteLine("Warning Sent");
+                }
+                try
+                {
+                    current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"{current.RemoteEndPoint} disconnected");
+                    current.Shutdown(SocketShutdown.Both);
+                    current.Close();
+                    clientSockets.Remove(current);
                 }
             }
-            else if (text == "exit")
-            {
-                current.Shutdown(SocketShutdown.Both);
-                current.Close();
-                clientSockets.Remove(current);
-                Console.WriteLine($"{current.RemoteEndPoint} disconnected");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("Text is an invalid request");
-                byte[] data = Encoding.ASCII.GetBytes("Invalid Request");
-                current.Send(data);
-                Console.WriteLine("Warning Sent");
-            }
-            try
-            {
-                current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine($"{current.RemoteEndPoint} disconnected");
-                current.Shutdown(SocketShutdown.Both);
-                current.Close();
-                clientSockets.Remove(current);
-            }
         }
+          
 
         private static string ConvertString(char[,] points)
         {
